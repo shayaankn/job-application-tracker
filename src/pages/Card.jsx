335b7f0e56fc9ebe52d8601bda8c80e117a80690
shayaankn <shayaankhan11@gmail.com>
2026-01-10 00:05:51 +0530

    update card component: remove the drag button, make the whole card draggable

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -15,34 +15,24 @@ export default function Card({ card, columnId }) {
   return (
     <div
       ref={setNodeRef}
+      {...listeners}
+      {...attributes}
       style={style}
-      className="bg-gray-50 border border-gray-300 rounded-md p-3 mb-3 flex items-center justify-between"
+      className="bg-gray-50 border border-gray-300 rounded-md p-3 mb-3 cursor-pointer"
     >
-      {/* Drag handle */}
-      <div
-        {...listeners}
-        {...attributes}
-        className="cursor-grab flex-shrink-0 mr-2"
-      >
-        <i className="bi bi-grip-vertical text-gray-400"></i>
-      </div>
-
-      {/* Card details */}
-      <div className="flex-1">
-        <h3 className="font-semibold text-gray-800 mb-1">{card.position}</h3>
-        <p className="text-gray-600 text-sm mb-1">
-          <i class="bi bi-buildings-fill text-gray-400 text-xs mr-2"></i>
-          {card.company}
-        </p>
-        <p className="text-gray-600 text-sm mb-1">
-          <i class="bi bi-geo-alt-fill text-gray-400 text-xs mr-2"></i>
-          {card.location} • {card.workMode}
-        </p>
-        <p className="text-gray-600 text-sm">
-          <i class="bi bi-wallet-fill text-gray-400 text-xs mr-2"></i>
-          {card.salaryRange}
-        </p>
-      </div>
+      <h3 className="font-semibold text-gray-800 mb-1">{card.position}</h3>
+      <p className="text-gray-600 text-sm mb-1">
+        <i class="bi bi-buildings-fill text-gray-400 text-xs mr-2"></i>
+        {card.company}
+      </p>
+      <p className="text-gray-600 text-sm mb-1">
+        <i class="bi bi-geo-alt-fill text-gray-400 text-xs mr-2"></i>
+        {card.location} • {card.workMode}
+      </p>
+      <p className="text-gray-600 text-sm">
+        <i class="bi bi-wallet-fill text-gray-400 text-xs mr-2"></i>
+        {card.salaryRange}
+      </p>
     </div>
   );
 }
